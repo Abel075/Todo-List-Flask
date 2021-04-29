4d7a4290b60fc6a72f3a18b572ec95cddf13f467
Abel075 <abeld.correa@gmail.com>
2021-04-29 18:04:06 -0300

    prueba

--- todo/__init__.py
+++ todo/__init__.py
@@ -2,3 +2,24 @@
 
 import os
 from flask import Flask
+
+def create_app():
+    app = Flask(__name__)
+
+
+    app.config.from_mapping(
+
+        SECRET_KEY='mikey',
+        DATABASE_HOST=os.environ.get('FLASK_DATABASE_HOST'),
+        DATABASE_PASSSWORD=os.environ.get('FLASK_DATABASE_PASSWORD'),
+        DATABASE_USER=os.environ.get('FLASK_DATABASE_USER'),
+        DATABASE=os.environ.get('FLASK_DATABASE'),
+    )
+
+    # Ruta de prueba
+
+    @app.route('/hola')
+    def hola():
+        return 'Prueba de desarrollo'
+
+    return app
